d911f79b1847f788e080f7d1a3d686d374492a17
Kristen Kulha and Megan Olson <megan@>
2017-10-16 13:21:45 -0700

    add all cities to array

--- js/pandemic.js
+++ js/pandemic.js
@@ -1,6 +1,6 @@
 export class Pandemic {
   constructor(){
-    this.cities = [["Atlanta", 0], ["Cairo", 0], ["Berlin", 0], ["Beijing", 0], ["Santiago", 0], ["Lagos", 0]];
+    this.cities = [["Atlanta", 0], ["Cairo", 0], ["Berlin", 0], ["Beijing", 0], ["Santiago", 0], ["Lagos", 0], ["Algiers", 0], ["Baghdad", 0], ["Bangkok", 0], ["Bogota", 0], ["Buenos Aries", 0], ["Chennai", 0], ["Chicago", 0], ["Delhi", 0], ["Essen", 0], ["Ho Chi Minh City", 0], ["Hong Kong", 0], ["Instanbul", 0], ["Jakarta", 0], ["Johannesburg", 0], ["Karachi", 0], ["Khatoum", 0], ["Kinshasa", 0], ["Kolkata", 0], ["Lima", 0], ["London", 0], ["Los Angeles", 0], ["Madrid", 0], ["Manila", 0], ["Mexico City", 0], ["Miami", 0], ["Milan", 0], ["Montreal", 0], ["Moscow", 0], ["Mumbai", 0], ["New York", 0], ["Osaka", 0], ["Paris", 0], ["Riyadh", 0], ["San Francisco", 0], ["Sao Paulo", 0], ["Seoul", 0], ["Shanghai", 0], ["St. Petersburg", 0], ["Sydney", 0], ["Taipei", 0], ["Tehran", 0], ["Tokyo", 0], ["Washington", 0]];
     this.infectedCities = 0;
   }
 
@@ -29,14 +29,6 @@ export class Pandemic {
       }
     }
   }
-
-  // infectCity() {
-  //   setInterval(() => {
-  //     let index = this.getRandomInt(0, 6);
-  //     this.cities[index][1] += 1;
-  //     this.infectedCities += 1;
-  //   }, 3000);
-  // }
 
   infectCity() {
     let index = this.getRandomInt(0, 6);
