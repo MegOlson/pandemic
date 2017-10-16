c154285c2408f6f86bb7cafc5c829c13db050a74
Kristen Kulha and Megan Olson <kristen@>
2017-10-16 10:00:10 -0700

    Will add 3 to infection level of 3 cities on the first turn.

--- js/pandemic.js
+++ js/pandemic.js
@@ -1,5 +1,27 @@
 export class Pandemic {
   constructor(){
-    this.cities = ["Atlanta", "Cairo", "Berlin", "Beijing", "Santigago", "Lagos"];
+    this.cities = [["Atlanta", 0], ["Cairo", 0], ["Berlin", 0], ["Beijing", 0], ["Santigago", 0], ["Lagos", 0]];
+    this.infectedCities = 0;
   }
+
+  start(){
+    for(let i = 0; i < 3; i ++) {
+      let index = this.getRandomInt(0, 6);
+      if(this.cities[index][1] == 0) {
+        this.cities[index][1] += 3;
+      }else {
+        i --;
+      }
+    }
+    this.infectedCities += 3;
+  }
+
+  getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+
+
 }
--- spec/pandemic-spec.js
+++ spec/pandemic-spec.js
@@ -3,15 +3,17 @@ import { Pandemic } from "./../js/pandemic.js";
 describe("Pandemic", function () {
   let pandemic = new Pandemic;
 
-  beforeEach(function(){
-    jasmine.clock().install();
-  });
+  // beforeEach(function(){
+  //   jasmine.clock().install();
+  // });
 
   it("creates an instance of Pandemic class", function() {
-    expect(pandemic.cities.includes("Atlanta")).toEqual(true);
+    expect(pandemic.cities[1].includes("Cairo")).toEqual(true);
   });
 
-  // it("adds 3 to infection level of 3 cities on first turn", function() {
-  //   expect(pandemic.start).toEqual(true);
-  // });
+  it("adds 3 to infection level of 3 cities on first turn", function() {
+    pandemic.start();
+    console.log(pandemic.cities);
+    expect(pandemic.infectedCities).toEqual(3);
+  });
 });
